d75532394cf1488d9923dee8841c033a8514a2cf
Eric Youngson <eayoungs@gmail.com>
2018-03-26 21:34:32 -0700

    Syntax

--- data_client.py
+++ data_client.py
@@ -5,7 +5,7 @@ HOST = '192.168.0.105'
 PORT = 80
 BUFFER_SZ = 4096
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as skt:
     skt.connect((HOST, PORT))
     skt.sendall("GET / HTTP/1.1\r\n\r\n")
     data = skt.recv(BUFFER_SZ)
