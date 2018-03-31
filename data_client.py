144f503a036c48ec0fb7a4ba7f326f33af63729f
Eric Youngson <eayoungs@gmail.com>
2018-03-30 23:13:47 -0700

    Added timestamp to readings, attempted newline

--- data_client.py
+++ data_client.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import datetime
 
 
 HOST = '192.168.0.103'
@@ -9,9 +10,11 @@ BUFFER_SZ = 4096
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as skt:
     skt.connect((HOST, PORT))
     skt.sendall(b"GET / HTTP/1.1\r\n\r\n")
-    data = str(skt.recv(BUFFER_SZ))
+    data = skt.recv(BUFFER_SZ)
 
-print('Received', repr(data))
+reading = {}
+json_response = json.loads(data)
+reading[datetime.datetime.now().isoformat()] = json_response
 
-with open('esp_moist.log', mode='w', encoding='utf-8') as f:
-    json.dump(data, f)
+with open('esp_moist.log', mode='a', encoding='utf-8') as f:
+    json.dump(str(reading)+'\n', f)
