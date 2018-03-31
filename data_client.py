71deb93d93cfa79ff0696904baf2a5a55d526770
Eric Youngson <eayoungs@gmail.com>
2018-03-30 22:58:10 -0700

    Append data to file

--- data_client.py
+++ data_client.py
@@ -1,13 +1,17 @@
 import socket
+import json
 
 
-HOST = '192.168.0.111'
+HOST = '192.168.0.103'
 PORT = 80
 BUFFER_SZ = 4096
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as skt:
     skt.connect((HOST, PORT))
     skt.sendall(b"GET / HTTP/1.1\r\n\r\n")
-    data = skt.recv(BUFFER_SZ)
+    data = str(skt.recv(BUFFER_SZ))
 
 print('Received', repr(data))
+
+with open('esp_moist.log', mode='w', encoding='utf-8') as f:
+    json.dump(data, f)
